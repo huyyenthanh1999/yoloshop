3caf8971839fbeb8b549d57543cd10c2a90f6e4e
CuongNvcqht3012002 <nguyencuong3012002@gmail.com>
2021-09-28 21:43:28 +0700

    Fix

--- server.js
+++ server.js
@@ -1,43 +1,39 @@
-require('dotenv').config()
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const mongoose = require('mongoose')
+// require('dotenv').config()
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 
-const app = express()
+const app = express();
 
 // add middleware
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser())
-
-
 
 // setup view
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 
-
 // setup public folder
-app.use(express.static('public'))
+// app.use(express.static('public'))
+app.use("/public",express.static(path.join(__dirname, 'public')));
+
 
 
 
 // connect to database
-async function connectDB(){
-    await mongoose.connect(
-        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.k7qck.mongodb.net/k14shop?retryWrites=true&w=majority`
-    )
-}
-connectDB()
-
+// mongoose.connect(
+//     `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.k7qck.mongodb.net/k14shop?retryWrites=true&w=majority`
+// )
 
 
 // setup router
-// product
-const productRoute = require('./routes/productRoute')
-app.use('/products', productRoute)
+const indexRoute = require('./routes/indexRoute');
+app.use('/', indexRoute)
 
+// product
+// const productRoute = require('../routes/productRoute')
+// app.use('/products', productRoute)
 
 
 // auth
@@ -45,20 +41,16 @@ app.use('/products', productRoute)
 
 
 
+
 // user
-const userRoute = require('./routes/userRoute')
-app.use('/users', userRoute)
+// ...
 
 
 
 
-// test server
-app.get('/test', (req, res) => {
-    res.json('Test thanh cong')
-})
 
 // listening
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-	console.log('Listening...')
+    console.log('Listening...')
 })
