eef8a338b79db27cc32aa0de39e3d7f9dd5e96a1
sonnv <sonnv.iddc@gmail.com>
2021-10-11 22:29:00 +0700

    ok

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,46 +1,48 @@
-const Cart = require('../models/CartModel')
+const Cart = require("../models/CartModel");
 
 module.exports.renderCart = (req, res) => {
-	res.render('pages/cart')
-}
+  res.render("pages/cart");
+};
 
 module.exports.loadCart = async (req, res) => {
-	try {
-		// find theo id
-        const carts = await Cart.find({ });
+  try {
+    // find theo id
+    const carts = await Cart.find({});
 
-		// res.status(200).json({
-		// 	status: 'success',
-        //     carts,
-		// })
-        // res.render('pages/cart', {
-        //     carts
-        // });
-	} catch (error) {
-		res.status(500).json({
-			status: 'fail',
-			message: 'Lỗi server',
-		})
-	}
-}
+    // res.status(200).json({
+    // 	status: 'success',
+    //     carts,
+    // })
+    // res.render('pages/cart', {
+    //     carts
+    // });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: "Lỗi server",
+    });
+  }
+};
 
-// module.exports.getDetailCart = async (req, res) => {
-//     try {
-//         const cart = await Cart.find({ _id:'615a49d9ff54f53a4fa59382' }).populate('userId').populate({ path: 'list', populate: { path: 'productId' } });
-//         // res.render('pages/cart', {
-//         //     cart: cart,
-//         // });
-//         res.status(200).json({
-// 			status: 'success',
-// 			cart,
-// 		})
-//     } catch (error) {
-//         res.status(500).json({
-// 			status: 'fail',
-// 			message: 'Lỗi server',
-// 		})
-//     }
-// }
+module.exports.getDetailCart = async (req, res) => {
+  try {
+    const cart = await Cart.find({ _id: "615a49d9ff54f53a4fa59382" })
+    //   .populate("userId")
+      .populate({ path: "list", populate: { path: "productId" } });
+    // res.render('pages/cart', {
+    //     cart: cart,
+    // });
+    res.status(200).json({
+      status: "success",
+      cart,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: "Lỗi server",
+    });
+  }
+};
 
 // module.exports.create = async (req, res) => {
 // 	// check if cart is exists
@@ -48,10 +50,10 @@ module.exports.loadCart = async (req, res) => {
 // 		const { email, phoneNumber } = req.body
 // 		const user = await User.findOne({ $or: [{ email }, { phoneNumber }] })
 
-// 		if (user) 
-// 			return res.status(400).json({ 
-//                 status: 'fail', 
-//                 message: 'User đã tồn tại.' 
+// 		if (user)
+// 			return res.status(400).json({
+//                 status: 'fail',
+//                 message: 'User đã tồn tại.'
 //             })
 
 // 		// hash password and register user
@@ -77,84 +79,84 @@ module.exports.loadCart = async (req, res) => {
 // }
 
 module.exports.updateCart = async (req, res) => {
-	const cartId = req.params.id
-	try {
-		const cart = await Cart.findByIdAndUpdate( { _id: cartId }, req.body)
-		if (!cart)
-			return res.status(400).json({
-				status: 'fail',
-				message: 'Không tìm thấy cart',
-			})
+  const cartId = req.params.id;
+  try {
+    const cart = await Cart.findByIdAndUpdate({ _id: cartId }, req.body);
+    if (!cart)
+      return res.status(400).json({
+        status: "fail",
+        message: "Không tìm thấy cart",
+      });
 
-		res.status(200).json({
-			status: 'success',
-            cart,
-			// data: { cart },
-		})
-	} catch (error) {
-		res.status(500).json({
-			status: 'fail',
-			message: 'Lỗi server',
-		})
-	}
-}
+    res.status(200).json({
+      status: "success",
+      cart,
+      // data: { cart },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: "Lỗi server",
+    });
+  }
+};
 
 module.exports.deleteCart = async (req, res) => {
-	const cartId = req.params.id
-	try {
-		const cart = await Cart.findByIdAndDelete({ _id: cartId })
-		if (!cart)
-			return res.status(400).json({
-				status: 'fail',
-				message: 'Không tìm thấy cart',
-			})
+  const cartId = req.params.id;
+  try {
+    const cart = await Cart.findByIdAndDelete({ _id: cartId });
+    if (!cart)
+      return res.status(400).json({
+        status: "fail",
+        message: "Không tìm thấy cart",
+      });
 
-		res.status(200).json({
-			status: 'success',
-			message: 'Xóa cart thành công',
-		})
-	} catch (error) {
-		res.status(500).json({
-			status: 'fail',
-			message: 'Lỗi server',
-		})
-	}
-}
+    res.status(200).json({
+      status: "success",
+      message: "Xóa cart thành công",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: "Lỗi server",
+    });
+  }
+};
 
-module.exports.getDetailCart = async (req, res) => {
-	const cartId = req.params.id
-    try {
-        const cart = await Cart.findById({ _id: cartId });
-        if (!cart)
-			return res.status(400).json({
-				status: 'fail',
-				message: 'Không tìm thấy cart',
-			})
+// module.exports.getDetailCart = async (req, res) => {
+//   const cartId = req.params.id;
+//   try {
+//     const cart = await Cart.findById({ _id: cartId });
+//     if (!cart)
+//       return res.status(400).json({
+//         status: "fail",
+//         message: "Không tìm thấy cart",
+//       });
 
-		res.status(200).json({
-			status: 'success',
-			cart,
-		})
-    } catch (error) {
-        res.status(500).json({
-			status: 'fail',
-			message: 'Lỗi server',
-		})
-    }
-}
+//     res.status(200).json({
+//       status: "success",
+//       cart,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       status: "fail",
+//       message: "Lỗi server",
+//     });
+//   }
+// };
 
 module.exports.getAllCart = async (req, res) => {
-    try {
-        const carts = await Cart.find({});
-        
-        res.status(200).json({
-			status: 'success',
-			carts,
-		})
-    } catch (error) {
-        res.status(500).json({
-			status: 'fail',
-			message: 'Lỗi server',
-		})
-    }
-}
+  try {
+    const carts = await Cart.find({});
+
+    res.status(200).json({
+      status: "success",
+      carts,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: "Lỗi server",
+    });
+  }
+};
--- public/js/cart.js
+++ public/js/cart.js
@@ -1,3 +1,4 @@
+async function render() {
 // bien du lieu
 let listData = undefined;
 
@@ -10,10 +11,13 @@ let listData = undefined;
 // listData = fetch('/api/carts/:id').then
 // Lay localStorage
 listData = JSON.parse(localStorage.getItem("listData")) || [];
+
+document.querySelector('#cart .cart__info .cart__info__txt p span ').innerText = 10
+
 let detailProducts = []
 listData.forEach((item) => {
     $.ajax({
-        url: `/api/products/${item.productId}`,
+        url: `/products/api/${item.productId}`,
         type: 'GET',
     })
     .then((data) => {
@@ -23,10 +27,24 @@ listData.forEach((item) => {
         })
     })
     .catch((error) => {
-        console.log(error);
+        console.log(error)
     })
 })
 console.log(detailProducts);
+}
+
+render()
 
 // an tien hanh dat hang => kiem tra dang nhap or not
 // window
+function order() {
+    // 
+}
+
+const btn_cart = document.querySelector(".btn-cart");
+btn_cart.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = "/";
+})
+// btn_cart.addEventListener('click', )
+
