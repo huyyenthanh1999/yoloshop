d57f9d109a05591f7ee29d4f2bba71b5983fa599
CuongNvcqht3012002 <nguyencuong3012002@gmail.com>
2021-10-22 14:55:55 +0700

    ok

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+const router = require('express').Router()
+
+// auth
+const authRoute = require('./authRoute')
+router.use('/auth', authRoute)
+
+// home page
+const indexRoute = require('./indexRoute')
+router.use('/', indexRoute)
+
+
+// products
+const productRoute = require('./productRoute')
+router.use('/products', productRoute)
+
+
+// account
+const accountRoute = require('./accountRoute')
+router.use('/account', accountRoute)
+
+
+// cart
+const cartRoute = require('./cartRoute')
+router.use('/cart', cartRoute)
+
+
+// checkout
+const checkoutRoute = require('./checkoutRoute')
+router.use('/checkout', checkoutRoute)
+
+
+// admin
+const adminRoute = require("./adminRoute");
+router.use("/admin", adminRoute);
+
+
+// orders
+const orderRoute = require('./orderRoute')
+router.use('/orders', orderRoute)
+
+
+// Not found page
+router.use((req, res) => res.render('pages/notfound'))
+
+module.exports = router
--- server.js
+++ server.js
@@ -13,14 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 // session middleware
-// app.use(
-// 	session({
-// 		secret: process.env.SESSION_SECRET,
-// 		resave: false,
-// 		saveUninitialized: true,
-// 		name: 'sessionId'
-// 	})
-// )
+app.use(require('./config/session'))
 
 // setup view
 app.set('view engine', 'ejs')
@@ -34,50 +27,10 @@ app.use('/public', express.static(path.join(__dirname, 'public')))
 // connect mongodb
 require('./config/connectDB')
 
-// index
-const indexRoute = require('./routes/indexRoute')
-app.use('/', indexRoute)
+// setup router
+const router = require('./routes')
+app.use(router)
 
-// product
-const productRoute = require('./routes/productRoute')
-app.use('/products', productRoute)
-
-// user
-const userRoute = require('./routes/userRoute')
-app.use('/users', userRoute)
-
-// auth
-const authRoute = require('./routes/authRoute')
-app.use('/auth', authRoute)
-
-// account
-const accountRoute = require('./routes/accountRoute')
-app.use('/user/account', accountRoute)
-
-// cart
-const cartRoute = require('./routes/cartRoute')
-app.use('/cart', cartRoute)
-
-// checkout
-const checkoutRoute = require('./routes/checkoutRoute')
-app.use('/checkout', checkoutRoute)
-
-// admin
-const adminRoute = require('./routes/adminRoute')
-app.use('/admin', adminRoute)
-
-// Products detail
-// const catalogsRoute = require('./routes/catalogsRoute')
-// app.use('/catalogs', catalogsRoute)
-
-// // detail product
-// app.get('/products/123', (req, res) => {
-// 	// console.log(req.query)
-// 	res.render('pages/detail-product')
-// })
-
-// Not found
-app.use((req, res) => res.render('pages/notfound'))
 
 // listening
 const PORT = process.env.PORT || 3000
