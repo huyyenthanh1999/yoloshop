a158e1ae669b1b2edfd4e73795792cfbaa6e08c0
CuongNvcqht3012002 <nguyencuong3012002@gmail.com>
2021-11-08 01:24:49 +0700

    ok

--- middlewares/getNum.js
+++ middlewares/getNum.js
@@ -1,4 +1,4 @@
-const Cart = require('../models/cartModel');
+// const Cart = require('../models/cartModel');
 const jwt = require('jsonwebtoken')
 
 module.exports.getLog = async (req, res, next) => {
