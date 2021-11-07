53acb78a36fceb55229126cdaf06bbdcfd71c31f
CuongNvcqht3012002 <nguyencuong3012002@gmail.com>
2021-11-08 01:26:31 +0700

    ok

--- middlewares/getNum.js
+++ middlewares/getNum.js
@@ -1,23 +1,23 @@
-// const Cart = require('../models/cartModel');
+const Cart = require('../models/cartModel');
 const jwt = require('jsonwebtoken')
 
 module.exports.getLog = async (req, res, next) => {
     try {
-        // const user = req.user;
-        // if(user){
-        //     const cartNum = await Cart.findOne({userId: user._id}).lean()
-        //     res.locals.cartNum = {
-        //         cartNum:cartNum
-        //     }
-        //     res.locals.logging = true;
-        // }else{
-        //     res.locals.cartNum = {
-        //         cartNum: {
-        //             products:[]
-        //         }
-        //     };
-        //     res.locals.logging = false;
-        // }
+        const user = req.user;
+        if(user){
+            const cartNum = await Cart.findOne({userId: user._id}).lean()
+            res.locals.cartNum = {
+                cartNum:cartNum
+            }
+            res.locals.logging = true;
+        }else{
+            res.locals.cartNum = {
+                cartNum: {
+                    products:[]
+                }
+            };
+            res.locals.logging = false;
+        }
         next()
     } catch (error) {
         console.log(error)
--- routes/index.js
+++ routes/index.js
@@ -5,56 +5,56 @@ const {checkUser} = require('../middlewares/checkUser')
 router.use(checkUser)
 
 const {getLog} = require('../middlewares/getNum');
-// router.use(getLog)
+router.use(getLog)
 // home page
-// const indexRoute = require('./indexRoute')
-// router.use('/', indexRoute)
+const indexRoute = require('./indexRoute')
+router.use('/', indexRoute)
 
-// // auth
-// const authRoute = require('./authRoute')
-// router.use('/auth', authRoute)
+// auth
+const authRoute = require('./authRoute')
+router.use('/auth', authRoute)
 
-// // for admin page to get products
-// const productRoute = require('./productRoute')
-// router.use('/products', productRoute)
+// for admin page to get products
+const productRoute = require('./productRoute')
+router.use('/products', productRoute)
 
-// // for products page
-// const catalogsRoute = require("./catalogsRoute");
-// router.use("/products", catalogsRoute);
+// for products page
+const catalogsRoute = require("./catalogsRoute");
+router.use("/products", catalogsRoute);
 
-// // account
-// const accountRoute = require('./accountRoute')
-// router.use('/account', accountRoute)
-
-
-// // cart
-// const cartRoute = require('./cartRoute')
-// router.use('/cart', cartRoute)
-
-// // checkout
-// const checkoutRoute = require('./checkoutRoute')
-// router.use('/checkout', checkoutRoute)
+// account
+const accountRoute = require('./accountRoute')
+router.use('/account', accountRoute)
 
 
-// // admin
-// const adminRoute = require("./adminRoute");
-// router.use("/admin", adminRoute);
+// cart
+const cartRoute = require('./cartRoute')
+router.use('/cart', cartRoute)
 
-// // users
-// const userRoute = require('./userRoute')
-// router.use("/users", userRoute)
+// checkout
+const checkoutRoute = require('./checkoutRoute')
+router.use('/checkout', checkoutRoute)
 
-// // orders
-// const orderRoute = require('./orderRoute')
-// router.use("/orders", orderRoute)
 
-// // contact
-// const contactRoute = require('./contactRoute')
-// router.use('/contact', contactRoute)
+// admin
+const adminRoute = require("./adminRoute");
+router.use("/admin", adminRoute);
 
-// //news
-// const newsRoute = require('./newsRoute.js')
-// router.use("/news", newsRoute)
+// users
+const userRoute = require('./userRoute')
+router.use("/users", userRoute)
+
+// orders
+const orderRoute = require('./orderRoute')
+router.use("/orders", orderRoute)
+
+// contact
+const contactRoute = require('./contactRoute')
+router.use('/contact', contactRoute)
+
+//news
+const newsRoute = require('./newsRoute.js')
+router.use("/news", newsRoute)
 
 // Not found page
 router.use((req, res) => res.render('pages/notfound'))
