8e5c2838f3573adcd40514eb56a147938494d3a9
CuongNvcqht3012002 <nguyencuong3012002@gmail.com>
2021-11-08 01:01:26 +0700

    ok

--- middlewares/getNum.js
+++ middlewares/getNum.js
@@ -1,28 +1,34 @@
-const Cart = require('../models/cartModel');
+const Cart = require('../models/cartModel')
 const jwt = require('jsonwebtoken')
 
 module.exports.getLog = async (req, res, next) => {
-    try {
-        // const user = req.user;
-        const user = 1
-        if(user){
-            // const cartNum = await Cart.findOne({userId: user._id}).lean()
+	try {
+		// const user = req.user;
+		// const user = 1
+		// if(user){
+		//     // const cartNum = await Cart.findOne({userId: user._id}).lean()
 
-            res.locals.cartNum = {
-                cartNum: 1
-            }
-            res.locals.logging = true;
-        }else{
-            res.locals.cartNum = {
-                cartNum: {
-                    products:[]
-                }
-            };
-            res.locals.logging = false;
-        }
-        next()
-    } catch (error) {
-        console.log(error)
-        next()
-    }
+		//     res.locals.cartNum = {
+		//         cartNum: 1
+		//     }
+		//     res.locals.logging = true;
+		// }else{
+		//     res.locals.cartNum = {
+		//         cartNum: {
+		//             products:[]
+		//         }
+		//     };
+		//     res.locals.logging = false;
+		// }
+		res.locals.cartNum = {
+			cartNum: {
+				products: [],
+			},
+		}
+		res.locals.logging = false
+		next()
+	} catch (error) {
+		console.log(error)
+		next()
+	}
 }
