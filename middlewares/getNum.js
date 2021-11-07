3d62f74f8595727b484c954e70352cd8b93ead7d
CuongNvcqht3012002 <nguyencuong3012002@gmail.com>
2021-11-08 01:23:39 +0700

    ok

--- middlewares/getNum.js
+++ middlewares/getNum.js
@@ -3,21 +3,21 @@ const jwt = require('jsonwebtoken')
 
 module.exports.getLog = async (req, res, next) => {
     try {
-        const user = req.user;
-        if(user){
-            const cartNum = await Cart.findOne({userId: user._id}).lean()
-            res.locals.cartNum = {
-                cartNum:cartNum
-            }
-            res.locals.logging = true;
-        }else{
-            res.locals.cartNum = {
-                cartNum: {
-                    products:[]
-                }
-            };
-            res.locals.logging = false;
-        }
+        // const user = req.user;
+        // if(user){
+        //     const cartNum = await Cart.findOne({userId: user._id}).lean()
+        //     res.locals.cartNum = {
+        //         cartNum:cartNum
+        //     }
+        //     res.locals.logging = true;
+        // }else{
+        //     res.locals.cartNum = {
+        //         cartNum: {
+        //             products:[]
+        //         }
+        //     };
+        //     res.locals.logging = false;
+        // }
         next()
     } catch (error) {
         console.log(error)
