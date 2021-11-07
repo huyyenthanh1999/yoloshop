9a21e37fb6c20e80cc7fc3d6fc8680f7571ccf70
CuongNvcqht3012002 <nguyencuong3012002@gmail.com>
2021-11-08 01:28:36 +0700

    ok

--- routes/index.js
+++ routes/index.js
@@ -4,41 +4,41 @@ const {checkUser} = require('../middlewares/checkUser')
 // gán req.user
 router.use(checkUser)
 
-const {getLog} = require('../middlewares/getNum');
-router.use(getLog)
+// const {getLog} = require('../middlewares/getNum');
+// router.use(getLog)
 // home page
-const indexRoute = require('./indexRoute')
-router.use('/', indexRoute)
+// const indexRoute = require('./indexRoute')
+// router.use('/', indexRoute)
 
 // auth
-const authRoute = require('./authRoute')
-router.use('/auth', authRoute)
+// const authRoute = require('./authRoute')
+// router.use('/auth', authRoute)
 
 // for admin page to get products
-const productRoute = require('./productRoute')
-router.use('/products', productRoute)
+// const productRoute = require('./productRoute')
+// router.use('/products', productRoute)
 
-// for products page
-const catalogsRoute = require("./catalogsRoute");
-router.use("/products", catalogsRoute);
+// // for products page
+// const catalogsRoute = require("./catalogsRoute");
+// router.use("/products", catalogsRoute);
 
-// account
-const accountRoute = require('./accountRoute')
-router.use('/account', accountRoute)
-
-
-// cart
-const cartRoute = require('./cartRoute')
-router.use('/cart', cartRoute)
-
-// checkout
-const checkoutRoute = require('./checkoutRoute')
-router.use('/checkout', checkoutRoute)
+// // account
+// const accountRoute = require('./accountRoute')
+// router.use('/account', accountRoute)
 
 
-// admin
-const adminRoute = require("./adminRoute");
-router.use("/admin", adminRoute);
+// // cart
+// const cartRoute = require('./cartRoute')
+// router.use('/cart', cartRoute)
+
+// // checkout
+// const checkoutRoute = require('./checkoutRoute')
+// router.use('/checkout', checkoutRoute)
+
+
+// // admin
+// const adminRoute = require("./adminRoute");
+// router.use("/admin", adminRoute);
 
 // users
 const userRoute = require('./userRoute')
